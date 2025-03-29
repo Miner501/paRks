eb0e85cf3f9ebf6c9190bd99755d343fd19bfe0e
Miner501 <michael.gaston1999@gmail.com>
2025-03-29 16:11:06 +0100

    created (working) travel_zone function

--- R/junk.R
+++ R/junk.R
@@ -1,3 +1,7 @@
 # This is just a test script to see if the functions etc in the package run as expected.
-geocode_address("Heinestraße 11, Würzburg")
-geocode_coords()
+# devtools::document()
+# devtools::load_all()
+
+#address <- geocode_address("Heinestraße 11, Würzburg")
+#tz <- travel_zone(address, mode = "walk", distance_km = 1)
+#plot(sf::st_geometry(tz))
